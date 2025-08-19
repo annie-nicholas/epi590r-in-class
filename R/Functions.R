166056a549bd4d0ab1df3b60e1b3dbc46b126232
annie-nicholas <annabel.nicholas@emory.edu>
2025-08-19 14:18:54 -0400

    added functions

--- R/Functions.R
+++ R/Functions.R
@@ -9,3 +9,29 @@ square <- function(x) {
 square(3)
 square(53)
 53^2
+
+new_mean <- function(x) {
+	n <- length(x)
+	mean_value <- sum(x) / n
+	return(mean_value)
+}
+
+x <- c(1, 2, 3, 4, 5)
+new_mean(x)
+new_mean(c(1, 2, 3, 4, 5))
+
+add_two_numbers <- function(x,y){
+	sum <- x + y
+	return(sum)
+}
+
+add_two_numbers(4, 6)
+
+
+raise <- function(x, y) {
+	raise_value <- x^y
+	return(raise_value)
+}
+
+raise(5, 6)
+5^6
